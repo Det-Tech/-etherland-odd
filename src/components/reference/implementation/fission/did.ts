2e55eaf13963598fdab6c8fb4cca2984c092d257
Det-tech <proninkirilll228@gmail.com>
2024-11-20 22:16:06 -0500

    updated did

--- src/components/reference/implementation/fission/did.ts
+++ src/components/reference/implementation/fission/did.ts
@@ -11,10 +11,11 @@ export async function root(
   username: string
 ): Promise<string> {
   try {
-    console.log("did &&& %%", `_did.${username}.${endpoints.userDomain}`)
+    console.log("did &&& %% h", `_did.${username}.${endpoints.userDomain}`)
     const maybeDid = await DOH.lookupTxtRecord(`_did.${username}.${endpoints.userDomain}`)
     console.log("maybeDid", maybeDid)
-    if (maybeDid !== null) return maybeDid
+    // if (maybeDid !== null) 
+    return `_did.${username}.${endpoints.userDomain}`
   } catch (_err) {
     console.log("err", _err)
     // lookup failed
